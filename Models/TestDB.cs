74e6feb5dcdd2eab2655b7a02546c79a260a4ba0
Dmitry-Dem <demenchuk.dmitry03@gmail.com>
2022-12-29 16:43:19 +0200

    Add project files.

---
+++ Models/TestDB.cs
@@ -0,0 +1,7 @@
+﻿namespace EpiChatApp.Models
+{
+	public static class TestDB
+	{
+		public static List<User> _users = new List<User>() { new User("Andre", 21, "log 1", "pass 1"), new User("Mishele", 19, "log 2", "pass 2"), new User("Dima", 19, "dddd", "dddd") };
+	}
+}
---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EpiChatApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email Addres")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
