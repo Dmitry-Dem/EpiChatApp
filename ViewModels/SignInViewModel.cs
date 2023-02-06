f35a1ce4eceebbefe960cedee6a0e1431112678d
Dmitry-Dem <demenchuk.dmitry03@gmail.com>
2023-02-06 16:44:08 +0200

    Added Sign Up page, redact SignIn/SignUp ViewModels

--- ViewModels/SignInViewModel.cs
+++ ViewModels/SignInViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace EpiChatApp.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EpiChatApp.ViewModels
 {
 	public class SignInViewModel
 	{
+		[Required(ErrorMessage = "Email is required")]
+		[EmailAddress]
+		public string? Email { get; set; }
+		[Required(ErrorMessage = "Password is required")]
+		[DataType(DataType.Password)]
+		public string? Password { get; set; }
 	}
 }
--- ViewModels/SignUpViewModel.cs
+++ ViewModels/SignUpViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace EpiChatApp.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EpiChatApp.ViewModels
 {
 	public class SignUpViewModel
 	{
+		[Required(ErrorMessage = "Name is required")]
+		public string? Name { get; set; }
+		[Required(ErrorMessage = "Email is required")]
+		[EmailAddress]
+		public string? Email { get; set; }
+		[Required(ErrorMessage = "Password is required")]
+		[DataType(DataType.Password)]
+		public string? Password { get; set; }
+		[Required(ErrorMessage = "Confirm Password is required")]
+		[Compare(nameof(Password), ErrorMessage = "Passwords don't match.")]
+		public string? PasswordConfirm { get; set; }
 	}
 }
